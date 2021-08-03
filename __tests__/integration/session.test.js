b91a4859fe400ca849c47864278ea4f9b644a119
iago-s <iagooliveira3845@hotmail.com>
2021-08-03 16:20:48 -0300

    feat: Add truncate before execute tests

--- __tests__/integration/session.test.js
+++ __tests__/integration/session.test.js
@@ -1,13 +1,27 @@
+const request = require('supertest');
+
+const app = require('../../src/app');
+const truncate = require('../utils/truncate');
 const { User } = require('../../src/app/models');
 
 describe('Authentication', () => {
-  it('should create user', async () => {
+  // Executa automaticamente antes de cada teste de session
+  beforeEach(async () => {
+    await truncate();
+  });
+
+  it('should authenticate with valid credentials', async () => {
     const user = await User.create({
-      name: 'User',
-      email: 'user@email.com',
+      name: 'Iago Silva',
+      email: 'iagosilva@email.com',
       password_hash: '123456',
     });
 
-    expect(user.email).toBe('user@email.com');
+    const response = await request(app).post('/sessions').send({
+      email: user.email,
+      password: '123456',
+    });
+
+    expect(response.status).toBe(200);
   });
 });
